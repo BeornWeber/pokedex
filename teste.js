12908e5fd80b1c024b1b830368e35962865e173e
Benjamim Arteaga Weber <182400531@senacinformatica.br>
2024-11-12 16:17:35 -0300

    base app

---
+++ teste.js
@@ -0,0 +1,37 @@
+async function getData() {
+    const url = "https://pokeapi.co/api/v2/pokemon";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+
+        const listContainer = document.getElementById('lista');
+
+        json.results.forEach(async (pokemon) => {
+
+            const currentPokemon = await fetch(pokemon.url) 
+            const currentPokemonJson = await currentPokemon.json()
+
+            console.log(currentPokemonJson)
+
+            const element = `
+                <li>
+                    <p>N°: ${currentPokemonJson.id}</p>
+                    <img src="${currentPokemonJson.sprites.other.home.front_default}" alt="foto" />
+                    <h2>${currentPokemonJson.name}</h2>
+                </li>
+                `
+
+            listContainer.innerHTML += element
+        });
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+getData()
